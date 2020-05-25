139add9c9659bcf52a112c89265f5fbdb7999b5b
liranl <liran.livne@ibm.com>
2020-05-25 10:37:14 +0300

    modifying block mixin

--- app/models/manageiq/providers/storage_manager/block_mixin.rb
+++ app/models/manageiq/providers/storage_manager/block_mixin.rb
@@ -7,6 +7,10 @@ module ManageIQ::Providers::StorageManager::BlockMixin
     has_many :cloud_volume_backups,   :foreign_key => :ems_id, :dependent => :destroy
     has_many :cloud_volume_types,     :foreign_key => :ems_id, :dependent => :destroy
 
+    # in order to see in the overview/dashboard page storages and pools we need to add them to the mixin
+    has_many :physical_storages, :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
+    has_many :storage_resources, :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
+
     supports :block_storage
   end
 end
