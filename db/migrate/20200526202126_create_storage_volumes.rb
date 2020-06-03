56333ff818bc7436965aace6dba1e35c9399d9ad
GREGORYBEYER <gregoryb@il.ibm.com>
2020-06-03 10:24:13 +0300

    SDE-1193: Added GTL view for storage-volume
    Fixed some migrations.
    Changed the menu to have CloudVolumes and PhysicalVolumes.
    Moved our client-patches to a central place so we can easily re-do them.

--- db/migrate/20200526202126_create_storage_volumes.rb
+++ db/migrate/20200526202126_create_storage_volumes.rb
@@ -3,9 +3,10 @@ class CreateStorageVolumes < ActiveRecord::Migration[5.1]
     create_table :storage_volumes do |t|
       t.string :name
       t.boolean :compliant
-      t.string :service_id
+      t.bigint :service_id
       t.bigint :size
-      t.string :storage_resource_id
+      t.bigint :storage_resource_id
+      t.bigint :storage_service_id
       t.string :uuid
       t.bigint :ems_id
       t.string :ems_ref
