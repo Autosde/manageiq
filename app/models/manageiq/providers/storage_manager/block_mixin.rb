25eada3d77479ed1f125d5a39d80796310e1a9fe
liranl <liran.livne@ibm.com>
2020-06-07 08:39:49 +0300

    SDE-1206: improve overview code

--- app/models/manageiq/providers/storage_manager/block_mixin.rb
+++ app/models/manageiq/providers/storage_manager/block_mixin.rb
@@ -2,14 +2,15 @@ module ManageIQ::Providers::StorageManager::BlockMixin
   extend ActiveSupport::Concern
 
   included do
-    has_many :cloud_volumes,          :foreign_key => :ems_id, :dependent => :destroy
+    has_many :cloud_volumes,      :foreign_key => :ems_id,  :dependent => :destroy
+    has_many :storage_volumes,    :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
+    has_many :storage_systems,    :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
+    has_many :storage_resources,  :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
+
+    # todo [liran] - move this into cinder-storage (this is not general block mixin props)
     has_many :cloud_volume_snapshots, :foreign_key => :ems_id, :dependent => :destroy
     has_many :cloud_volume_backups,   :foreign_key => :ems_id, :dependent => :destroy
     has_many :cloud_volume_types,     :foreign_key => :ems_id, :dependent => :destroy
-
-    # in order to see in the overview/dashboard page storages and pools we need to add them to the mixin
-    has_many :physical_storages, :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
-    has_many :storage_resources, :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
 
     supports :block_storage
   end
