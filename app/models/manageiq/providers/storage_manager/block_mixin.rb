e73f4d8a808d30c141777b52a63c83e3df7640d3
GREGORYBEYER <gregoryb@il.ibm.com>
2020-06-17 09:08:05 +0300

    SDE-1235: Attaching storage-system through MIQ
    Patched the autosde client.
    Fixed some minor bugs.

--- app/models/manageiq/providers/storage_manager/block_mixin.rb
+++ app/models/manageiq/providers/storage_manager/block_mixin.rb
@@ -6,6 +6,7 @@ module ManageIQ::Providers::StorageManager::BlockMixin
     has_many :storage_systems,    :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
     has_many :storage_resources,  :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
     has_many :storage_system_types, :foreign_key => :ems_id, :dependent => :destroy, :inverse_of => :ext_management_system
+    has_many :storage_services, :foreign_key => "ems_id", :dependent => :destroy, :inverse_of => :ext_management_system
 
     # todo [liran] - move this into cinder-storage (this is not general block mixin props)
     has_many :cloud_volume_snapshots, :foreign_key => :ems_id, :dependent => :destroy
