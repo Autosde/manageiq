89b157c96f21958b5f7a912e2409f528818adad5
Erez Theodorou <erezt@il.ibm.com>
2020-07-19 20:59:09 +0300

    SDE-1233 Add type to volumes - add volume_source field added to the cloud_volume schema a column named volume_source string field

--- db/migrate/20200806112501_expand_cloud_volume.rb
+++ db/migrate/20200806112501_expand_cloud_volume.rb
@@ -6,5 +6,6 @@ class ExpandCloudVolume < ActiveRecord::Migration[5.1]
     add_column :cloud_volumes, :storage_resource_id, :bigint
     add_column :cloud_volumes, :storage_service_id, :bigint
     add_column :cloud_volumes, :compliant, :boolean
+    add_column :cloud_volumes, :volume_source, :string
   end
 end
