847a7918a82c47c94288479c179cb8d635895e77
AutoSDE <autosde@il.ibm.com>
2020-07-30 10:12:13 +0300

    SDE-1283: Added manageiq-schema to default.xml

--- bundler.d/overrides.rb
+++ bundler.d/overrides.rb
@@ -2,3 +2,4 @@ override_gem "manageiq-ui-classic", path: "/var/www/miq/vmdb/plugins/manageiq-ui
 override_gem "manageiq-decorators", path: "/var/www/miq/vmdb/plugins/manageiq-decorators"
 override_gem "manageiq-providers-autosde", path:  "/var/www/miq/vmdb/plugins/manageiq-providers-autosde"
 override_gem "manageiq-core-autosde", path:  "/var/www/miq/vmdb/plugins/manageiq-core-autosde"
+override_gem "manageiq-schema", path:  "/var/www/miq/vmdb/plugins/manageiq-schema"
