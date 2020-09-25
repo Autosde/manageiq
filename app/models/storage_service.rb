662b1e254e90ac2d5c60ca12e0a9b7ff4ebe2138
GREGORYBEYER <gregoryb@il.ibm.com>
2020-09-25 14:16:15 +0300

    SDE-1400: Enabled adding storage-managers

--- app/models/storage_service.rb
+++ app/models/storage_service.rb
@@ -18,4 +18,107 @@ class StorageService < ApplicationRecord
     # side, that would return correct class for each provider
     ext_management_system && ext_management_system.class::StorageService
   end
+
+
+  def self.create_storage_service_queue(userid, ext_management_system, options = {})
+    task_opts = {
+      :action => "creating Cloud StorageService for user #{userid}",
+      :userid => userid
+    }
+
+    queue_opts = {
+      :class_name  => 'StorageService',
+      :method_name => 'create_storage_service',
+      :role        => 'ems_operations',
+      :queue_name  => ext_management_system.queue_name_for_ems_operations,
+      :zone        => ext_management_system.my_zone,
+      :args        => [ext_management_system.id, options]
+    }
+
+    MiqTask.generic_action_with_callback(task_opts, queue_opts)
+  end
+
+  def self.create_storage_service(ems_id, options = {})
+    raise ArgumentError, _("ems_id cannot be nil") if ems_id.nil?
+
+    ext_management_system = ExtManagementSystem.find(ems_id)
+    raise ArgumentError, _("ext_management_system cannot be found") if ext_management_system.nil?
+
+    klass = class_by_ems(ext_management_system)
+    klass.raw_create_storage_service(ext_management_system, options)
+  end
+
+  def self.validate_create_storage_service(ext_management_system)
+    klass = class_by_ems(ext_management_system)
+    return klass.validate_create_storage_service(ext_management_system) if
+        ext_management_system && klass.respond_to?(:validate_create_storage_service)
+
+    validate_unsupported("Create StorageService Operation")
+  end
+
+  def self.raw_create_storage_service(_ext_management_system, _options = {})
+    raise NotImplementedError, _("raw_create_storage_service must be implemented in a subclass")
+  end
+
+  def update_storage_service_queue(userid, options = {})
+    task_opts = {
+      :action => "updating StorageService for user #{userid}",
+      :userid => userid
+    }
+
+    queue_opts = {
+      :class_name  => self.class.name,
+      :method_name => 'update_storage_service',
+      :instance_id => id,
+      :role        => 'ems_operations',
+      :queue_name  => ext_management_system.queue_name_for_ems_operations,
+      :zone        => ext_management_system.my_zone,
+      :args        => [options]
+    }
+
+    MiqTask.generic_action_with_callback(task_opts, queue_opts)
+  end
+
+  def update_storage_service(options = {})
+    raw_update_storage_service(options)
+  end
+
+  def validate_update_storage_service
+    validate_unsupported("Update Volume Operation")
+  end
+
+  def raw_update_storage_service(_options = {})
+    raise NotImplementedError, _("raw_update_volume must be implemented in a subclass")
+  end
+
+  def delete_storage_service_queue(userid)
+    task_opts = {
+      :action => "deleting StorageService for user #{userid}",
+      :userid => userid
+    }
+
+    queue_opts = {
+      :class_name  => self.class.name,
+      :method_name => 'delete_storage_service',
+      :instance_id => id,
+      :role        => 'ems_operations',
+      :queue_name  => ext_management_system.queue_name_for_ems_operations,
+      :zone        => ext_management_system.my_zone,
+      :args        => []
+    }
+
+    MiqTask.generic_action_with_callback(task_opts, queue_opts)
+  end
+
+  def delete_storage_service
+    raw_delete_storage_service
+  end
+
+  def validate_delete_storage_service
+    validate_unsupported("Delete StorageService Operation")
+  end
+
+  def raw_delete_storage_service
+    raise NotImplementedError, _("raw_delete_storage_service must be implemented in a subclass")
+  end
 end
